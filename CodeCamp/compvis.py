806383b2c49976fbba05c31daa183073c6e57dce
isaacvock <isaac.vock@gmail.com>
2024-07-25 21:28:30 -0400

    Continued computer vision project

--- CodeCamp/compvis.py
+++ CodeCamp/compvis.py
@@ -126,3 +126,90 @@ class FashionMNISTModelV0(nn.Module):
     
     def forward(self, x):
         return self.layer_stack(x)
+    
+torch.manual_seed(42)
+
+model_0 = FashionMNISTModelV0(input_shape = 784,
+                              hidden_units = 10,
+                              output_shape = len(class_names))
+
+model_0.to("cpu")
+
+
+# Loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr = 0.1)
+
+### Custom accuracy function
+import requests
+from pathlib import Path 
+
+# Download helper functions from Learn PyTorch repo (if not already downloaded)
+if Path("helper_functions.py").is_file():
+  print("helper_functions.py already exists, skipping download")
+else:
+  print("Downloading helper_functions.py")
+  # Note: you need the "raw" GitHub URL for this to work
+  request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+  with open("helper_functions.py", "wb") as f:
+    f.write(request.content)
+
+### Strategy for timing to compare CPU vs. GPU speed
+from timeit import default_timer as timer
+def print_train_time(start: float, end: float,
+                     device: torch.device = None):
+    """Prints difference between start and end time.
+
+    Args:
+        start (float): Start time of computation (preferred in timeit format). 
+        end (float): End time of computation.
+        device ([type], optional): Device that compute is running on. Defaults to None.
+
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds") 
+    return total_time
+
+
+### Have to loop through batches and data
+
+# Progress bar spice
+from tqdm.auto import tqdm
+
+# Set seed and start the timer
+torch.manual_seed(42)
+train_time_start_on_cpu = timer()
+
+# number of epochs
+epochs = 3
+
+# Training and testing loop
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n------")
+    ### Training
+    train_loss = 0
+
+    # Loop through training batches
+    for batch, (X, y) in enumerate(train_dataloader):
+        model_0.train()
+        # 1. Forward pass
+        y_pred = model_0(X)
+
+        # 2. Calculate loss (per batch)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss # accumatively add up the loss per epoch
+
+        # 3. Zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Print out how many samples have been seen
+        if batch % 400 == 0:
+            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
