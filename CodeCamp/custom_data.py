eb787d51072caffeecc4fa5c8446094a5ee9c85d
isaacvock <isaac.vock@gmail.com>
2024-08-09 21:27:25 -0400

    Continued working through custom data loading compvision

--- CodeCamp/custom_data.py
+++ CodeCamp/custom_data.py
@@ -328,14 +328,15 @@ simple_transform = transforms.Compose([
 ])
 
 
-# 1) Turn each training and test folder into a Dataset
+# 1) TURN EACH TRAINING AND TEST FOLDER INTO A DATASET
+
 from torchvision import datasets
 train_data_simple = datasets.ImageFolder(root=train_dir,
                                          transform=simple_transform)
 test_data_simple = datasets.ImageFolder(root=test_dir,
                                         transform=simple_transform)
 
-# 2) Turn data into DataLoaders
+# 2) TURN DATA INTO DATALOADERS
 import os
 from torch.utils.data import DataLoader
 
@@ -353,7 +354,9 @@ test_dataloader_simple = DataLoader(test_data_simple,
                                     num_workers=NUM_WORKERS)
 
 
-# 3) Create TinyVGG model class
+# 3) CREATE TINYVGG MODEL CLASS
+
+from torch import nn
 class TinyVGG(nn.Module):
     def __init__(self, 
                  input_shape: int,
@@ -404,7 +407,7 @@ model_0 = TinyVGG(input_shape=3, # number of color channels (3 for RGB)
 model_0
 
 
-# 4) Test model by running forward pass on a single image
+# 4) TEST MODEL BY RUNNING FORWARD PASS ON A SINGLE IMAGE
 
 # a) Get a batch of images and labels from the DataLoader
 img_batch, label_batch = next(iter(train_dataloader_simple))
@@ -423,3 +426,171 @@ print(f"Output logits:\n{pred}\n")
 print(f"Output prediction probabilities:\n{torch.softmax(pred, dim=1)}\n")
 print(f"Output prediction label:\n{torch.argmax(torch.softmax(pred, dim=1), dim=1)}\n")
 print(f"Actual label:\n{label_single}")
+
+# Aside: use torchinfo to get an idea of the shapes going through our model
+from torchinfo import summary
+summary(model_0, input_size=[1, 3, 64, 64])
+
+
+# 5) CREATE TRAIN AND TEST LOOP
+
+# Train function
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               device: torch.device = device):
+    # Put model in train mode
+    model.train()
+
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Backpropogate
+        loss.backward()
+
+        # 5. Update parameters towards min loss
+        optimizer.step()
+
+        # Calculate and accumulate accuracy metric acrpss all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim =1 ), dim = 1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+    
+    # Adjust metrics to get avg loss and acc per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+# Test function
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              device: torch.device = device):
+    # Put model in eval mode
+    model.eval()
+
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            test_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            # 3. Calculate accuracy and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+
+    # Adjust metrics to get average loss and accuracy per batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+# Full training function
+from tqdm.auto import tqdm
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5,
+          device: torch.device = device):
+    
+    # Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+               }
+    
+    # Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model = model,
+                                           dataloader = train_dataloader,
+                                           loss_fn = loss_fn,
+                                           optimizer = optimizer,
+                                           device = device)
+        test_loss, test_acc = test_step(model = model,
+                                        dataloader = test_dataloader,
+                                        loss_fn = loss_fn,
+                                        device = device)
+        
+        # Print what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+
+    return results
+
+
+# 6) TRAIN AND EVALUATE MODEL 0
+
+# 92 second training time with cuda
+device = "cpu"
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+NUM_EPOCHS = 5
+
+# Recreate model instance
+model_0 = TinyVGG(
+    input_shape=3,
+    hidden_units=10,
+    output_shape=len(train_data.classes)).to(device)
+
+# Setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+
+# Start timer
+from timeit import default_timer as timer
+start_time = timer()
+
+# Train model 0
+model_0_results = train(model=model_0,
+                        train_dataloader=train_dataloader_simple,
+                        test_dataloader=test_dataloader_simple,
+                        optimizer=optimizer,
+                        loss_fn=loss_fn,
+                        epochs=NUM_EPOCHS)
+
+end_time = timer()
+print(f"Total training time: {end_time-start_time:.3f} seconds")
