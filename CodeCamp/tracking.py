6bfcc6d3fb66e9568cc40fafc15e0c1384b99987
isaacvock <isaac.vock@gmail.com>
2024-08-25 22:03:47 -0400

    Learned more about experiment tracking with tensorboard

--- CodeCamp/tracking.py
+++ CodeCamp/tracking.py
@@ -1,4 +1,6 @@
 import torch
+import torch.utils
+import torch.utils.tensorboard
 import torchvision
 import matplotlib.pyplot as plt
 
@@ -204,3 +206,413 @@ results = train(model = model,
                 loss_fn=loss_fn,
                 epochs=5,
                 device=device)
+
+
+##### STEP 4: Helper function to build SummaryWriter() instance
+
+# Create helper function
+def create_writer(experiment_name: str,
+                  model_name: str,
+                  extra: str=None) -> torch.utils.tensorboard.writer.SummaryWriter():
+    
+    """Creates a torch.utils.tensorboard.writer.SummaryWriter() instance saving to a specific log_dir.
+
+    log_dir is a combination of runs/timestamp/experiment_name/model_name/extra.
+
+    Where timestamp is the current date in YYYY-MM-DD format.
+
+    Args:
+        experiment_name (str): Name of experiment.
+        model_name (str): Name of model.
+        extra (str, optional): Anything extra to add to the directory. Defaults to None.
+
+    Returns:
+        torch.utils.tensorboard.writer.SummaryWriter(): Instance of a writer saving to log_dir.
+
+    Example usage:
+        # Create a writer saving to "runs/2022-06-04/data_10_percent/effnetb2/5_epochs/"
+        writer = create_writer(experiment_name="data_10_percent",
+                               model_name="effnetb2",
+                               extra="5_epochs")
+        # The above is the same as:
+        writer = SummaryWriter(log_dir="runs/2022-06-04/data_10_percent/effnetb2/5_epochs/")
+    """
+
+    from datetime import datetime
+    import os
+
+    # Get timestamp of current date
+    timestamp = datetime.now().strftime("%Y-%m-%d")
+
+    if extra:
+        log_dir = os.path.join("runs", 
+                               timestamp,
+                               experiment_name,
+                               model_name,
+                               extra)
+    else:
+        log_dir = os.path.join("runs",
+                               timestamp,
+                               experiment_name,
+                               model_name)
+        
+    print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
+    return SummaryWriter(log_dir = log_dir)
+
+
+example_writer = create_writer(
+    experiment_name='data_10_percent',
+    model_name="effnetb0",
+    extra="5_epochs"
+)
+
+# Update our train() function
+    # Want to create separate writers for
+    # each call to train(); we could be
+    # training multiple different models
+
+from typing import Dict, List
+from tqdm.auto import tqdm
+
+# Add writer parameter to train()
+def train(model: torch.nn.Module, 
+          train_dataloader: torch.utils.data.DataLoader, 
+          test_dataloader: torch.utils.data.DataLoader, 
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module,
+          epochs: int,
+          device: torch.device, 
+          writer: torch.utils.tensorboard.writer.SummaryWriter # new parameter to take in a writer
+          ) -> Dict[str, List]:
+    """Trains and tests a PyTorch model.
+
+    Passes a target PyTorch models through train_step() and test_step()
+    functions for a number of epochs, training and testing the model
+    in the same epoch loop.
+
+    Calculates, prints and stores evaluation metrics throughout.
+
+    Stores metrics to specified writer log_dir if present.
+
+    Args:
+      model: A PyTorch model to be trained and tested.
+      train_dataloader: A DataLoader instance for the model to be trained on.
+      test_dataloader: A DataLoader instance for the model to be tested on.
+      optimizer: A PyTorch optimizer to help minimize the loss function.
+      loss_fn: A PyTorch loss function to calculate loss on both datasets.
+      epochs: An integer indicating how many epochs to train for.
+      device: A target device to compute on (e.g. "cuda" or "cpu").
+      writer: A SummaryWriter() instance to log model results to.
+
+    Returns:
+      A dictionary of training and testing loss as well as training and
+      testing accuracy metrics. Each metric has a value in a list for 
+      each epoch.
+      In the form: {train_loss: [...],
+                train_acc: [...],
+                test_loss: [...],
+                test_acc: [...]} 
+      For example if training for epochs=2: 
+              {train_loss: [2.0616, 1.0537],
+                train_acc: [0.3945, 0.3945],
+                test_loss: [1.2641, 1.5706],
+                test_acc: [0.3400, 0.2973]} 
+    """
+    # Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+    }
+
+    # Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                          dataloader=train_dataloader,
+                                          loss_fn=loss_fn,
+                                          optimizer=optimizer,
+                                          device=device)
+        test_loss, test_acc = test_step(model=model,
+          dataloader=test_dataloader,
+          loss_fn=loss_fn,
+          device=device)
+
+        # Print out what's happening
+        print(
+          f"Epoch: {epoch+1} | "
+          f"train_loss: {train_loss:.4f} | "
+          f"train_acc: {train_acc:.4f} | "
+          f"test_loss: {test_loss:.4f} | "
+          f"test_acc: {test_acc:.4f}"
+        )
+
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+
+        ### New: Use the writer parameter to track experiments ###
+        # See if there's a writer, if so, log to it
+        if writer:
+            # Add results to SummaryWriter
+            writer.add_scalars(main_tag="Loss", 
+                               tag_scalar_dict={"train_loss": train_loss,
+                                                "test_loss": test_loss},
+                               global_step=epoch)
+            writer.add_scalars(main_tag="Accuracy", 
+                               tag_scalar_dict={"train_acc": train_acc,
+                                                "test_acc": test_acc}, 
+                               global_step=epoch)
+
+            # Close the writer
+            writer.close()
+        else:
+            pass
+    ### End new ###
+
+    # Return the filled results at the end of the epochs
+    return results
+
+
+##### STEP 5: SET UP SERIES OF MODELING EXPERIMENTS
+
+### Things we can try
+# 1) Different amounts of data
+# 2) Different (pre-trained) models
+# 3) Different training time
+
+# Download data wrapper function
+import os
+import zipfile
+
+from pathlib import Path
+
+import requests
+
+def download_data(source: str, 
+                  destination: str,
+                  remove_source: bool = True) -> Path:
+    """Downloads a zipped dataset from source and unzips to destination.
+
+    Args:
+        source (str): A link to a zipped file containing data.
+        destination (str): A target directory to unzip data to.
+        remove_source (bool): Whether to remove the source after downloading and extracting.
+    
+    Returns:
+        pathlib.Path to downloaded data.
+    
+    Example usage:
+        download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
+                      destination="pizza_steak_sushi")
+    """
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination
+
+    # If the image folder doesn't exist, download it and prepare it... 
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+        
+        # Download pizza, steak, sushi data
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as f:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            f.write(request.content)
+
+        # Unzip pizza, steak, sushi data
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...") 
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file
+        if remove_source:
+            os.remove(data_path / target_file)
+    
+    return image_path
+
+# Download 10 percent and 20 percent training data (if necessary)
+data_10_percent_path = download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
+                                     destination="pizza_steak_sushi")
+
+data_20_percent_path = download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi_20_percent.zip",
+                                     destination="pizza_steak_sushi_20_percent")
+
+
+# Filepaths for the different experiments
+train_dir_10_percent = data_10_percent_path / "train"
+train_dir_20_percent = data_20_percent_path / "train"
+test_dir = data_10_percent_path / "test"
+
+# Transforms
+from torchvision import transforms
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std = [0.229, 0.224, 0.225])
+
+simple_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    normalize
+])
+
+# DataLoaders
+BATCH_SIZE = 32
+
+train_dataloader_10_percent, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir_10_percent,
+    test_dir=test_dir, 
+    transform=simple_transform,
+    batch_size=BATCH_SIZE
+)
+
+train_dataloader_20_percent, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir_20_percent,
+    test_dir=test_dir,
+    transform=simple_transform,
+    batch_size=BATCH_SIZE
+)
+
+### Get the bigger pre-trained mode
+import torchvision
+from torchinfo import summary
+
+# Create instance of pre-trained model
+effnetb2_weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT # "DEFAULT" means best available weights
+effnetb2 = torchvision.models.efficientnet_b2(weights=effnetb2_weights)
+
+# Freeze parameters and change classifier head
+from torch import nn
+
+OUT_FEATURES = len(class_names)
+
+# Silly-ish seed setting function
+def set_seeds(seed: int=42):
+    """Sets random sets for torch operations.
+
+    Args:
+        seed (int, optional): Random seed to set. Defaults to 42.
+    """
+    # Set the seed for general torch operations
+    torch.manual_seed(seed)
+    # Set the seed for CUDA torch operations (ones that happen on the GPU)
+    torch.cuda.manual_seed(seed)
+
+def create_effnetb0():
+    # 1. Get the base mdoel with pretrained weights and send to target device
+    weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
+    model = torchvision.models.efficientnet_b0(weights=weights).to(device)
+
+    # 2. Freeze the base model layers
+    for param in model.features.parameters():
+        param.requires_grad = False
+
+    # 3. Set the seeds
+    set_seeds()
+
+    # 4. Change the classifier head
+    model.classifier = nn.Sequential(
+        nn.Dropout(p=0.2),
+        nn.Linear(in_features=1280, out_features=OUT_FEATURES)
+    ).to(device)
+
+    # 5. Give the model a name
+    model.name = "effnetb0"
+    print(f"[INFO] Created new {model.name} model.")
+    return model
+
+# Create an EffNetB2 feature extractor
+def create_effnetb2():
+    # 1. Get the base model with pretrained weights and send to target device
+    weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT
+    model = torchvision.models.efficientnet_b2(weights=weights).to(device)
+
+    # 2. Freeze the base model layers
+    for param in model.features.parameters():
+        param.requires_grad = False
+
+    # 3. Set the seeds
+    set_seeds()
+
+    # 4. Change the classifier head
+    model.classifier = nn.Sequential(
+        nn.Dropout(p=0.3),
+        nn.Linear(in_features=1408, out_features=OUT_FEATURES)
+    ).to(device)
+
+    # 5. Give the model a name
+    model.name = "effnetb2"
+    print(f"[INFO] Created new {model.name} model.")
+    return model
+
+effnetb0 = create_effnetb0() 
+effnetb2 = create_effnetb2()
+
+# Create experiments and set up training code
+num_epochs = [5, 10]
+
+models = ["effnetb0", "effnetb2"]
+
+train_dataloaders = {"data_10_percent": train_dataloader_10_percent,
+                     "data_20_percent": train_dataloader_20_percent}
+
+### The loop
+
+from going_modular.going_modular.utils import save_model
+
+# 1. Set the random seeds
+set_seeds(seed=42)
+
+# 2. Keep track of experiment numbers
+experiment_number = 0
+
+# 3. Loop through each DataLoader
+for dataloader_name, train_dataloader in train_dataloaders.items():
+
+    # 4. Loop through each number of epochs
+    for epochs in num_epochs: 
+
+        # 5. Loop through each model name and create a new model based on the name
+        for model_name in models:
+
+            # 6. Create information print outs
+            experiment_number += 1
+            print(f"[INFO] Experiment number: {experiment_number}")
+            print(f"[INFO] Model: {model_name}")
+            print(f"[INFO] DataLoader: {dataloader_name}")
+            print(f"[INFO] Number of epochs: {epochs}")  
+
+            # 7. Select the model
+            if model_name == "effnetb0":
+                model = create_effnetb0() # creates a new model each time (important because we want each experiment to start from scratch)
+            else:
+                model = create_effnetb2() # creates a new model each time (important because we want each experiment to start from scratch)
+            
+            # 8. Create a new loss and optimizer for every model
+            loss_fn = nn.CrossEntropyLoss()
+            optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+
+            # 9. Train target model with target dataloaders and track experiments
+            train(model=model,
+                  train_dataloader=train_dataloader,
+                  test_dataloader=test_dataloader, 
+                  optimizer=optimizer,
+                  loss_fn=loss_fn,
+                  epochs=epochs,
+                  device=device,
+                  writer=create_writer(experiment_name=dataloader_name,
+                                       model_name=model_name,
+                                       extra=f"{epochs}_epochs"))
+            
+            # 10. Save the model to file so we can get back the best model
+            save_filepath = f"07_{model_name}_{dataloader_name}_{epochs}_epochs.pth"
+            save_model(model=model,
+                       target_dir="models",
+                       model_name=save_filepath)
+            print("-"*50 + "\n")
