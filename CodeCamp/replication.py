1225eb7ab4482bda3ba0f2cd5d15fc71871bd2cb
isaacvock <isaac.vock@gmail.com>
2024-09-07 02:24:17 -0400

    Finished replicating ViT

--- CodeCamp/replication.py
+++ CodeCamp/replication.py
@@ -227,6 +227,8 @@ class MultiheadSelfAttentionBlock(nn.Module):
             need_weights = False
         )
 
+        return attn_output
+
 # Create an instance of MSABlock
 multihead_self_attention_block = MultiheadSelfAttentionBlock(embedding_dim=768, # from Table 1
                                                              num_heads=12) # from Table 1
@@ -297,3 +299,132 @@ class TransformerEncoderBlock(nn.Module):
         x = self.mlp_block(x) + x
 
         return x
+    
+test_data = torch.randn(1, 768)
+tform = TransformerEncoderBlock()
+tform(test_data)
+
+##### THE FULL SHEBANG
+
+class ViT(nn.Module):
+    def __init__(self,
+                 img_size:int=224,
+                 in_channels:int=3,
+                 patch_size=16,
+                 num_transformer_layers:int=12,
+                 embedding_dim:int=768,
+                 mlp_size:int=3072,
+                 num_heads:int=12,
+                 attn_dropout:float=0,
+                 mlp_dropout:float=0.1,
+                 embedding_dropout:float=0.1,
+                 num_classes:int=1000):
+        super().__init__()
+
+        # Make sure image size is divisible by patch size
+        assert img_size % patch_size == 0, f"Image size must be divisible by patch size, image size: {img_size}, patch size: {patch_size}."
+
+        # Calculate the number of patches (height * width/patch^2)
+        self.num_patches = ( img_size * img_size ) // patch_size ** 2
+
+        # Create learnable class embedding
+        self.class_embedding = nn.Parameter(data = torch.randn(1, 1, embedding_dim,
+                                                               requires_grad=True))
+        
+        # Create learnable position embedding
+        self.position_embedding = nn.Parameter(data=torch.randn(1, self.num_patches+1, embedding_dim),
+                                               requires_grad=True)
+        
+        # Create embedding dropout value
+        self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+
+        # Create patch embedding layer
+        self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                              patch_size=patch_size,
+                                              embedding_dim=embedding_dim)
+        
+        # Create Transformer Encoder blocks
+        # `*`` means "all"
+        self.transformer_encoder = nn.Sequential(
+            *[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                      num_heads=num_heads,
+                                      mlp_size=mlp_size,
+                                      mlp_dropout=mlp_dropout) for _ in range(num_transformer_layers)]
+        )
+        
+        # Create classifier head
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim,
+                      out_features=num_classes)             
+        )
+
+    # Create forward method
+    def forward(self, x):
+
+        # Get batch size
+        batch_size = x.shape[0]
+
+        # Create class token embedding and expand it to match the batch size
+        class_token = self.class_embedding.expand(batch_size, -1, -1) # -1 = infer dimension
+
+        # Create patch embedding
+        x = self.patch_embedding(x)
+
+        # Concat class embedding and patch embedding
+        x = torch.cat((class_token, x), dim = 1)
+
+        # Add position embedding to patch embedding
+        x = self.position_embedding + x
+
+        # Run embedding dropout
+        x = self.embedding_dropout(x)
+
+        # Pass embeddings through transformer
+        x = self.transformer_encoder(x)
+
+        # Put 0 index logit through classifier
+        x = self.classifier(x[:,0])
+
+        return x
+
+
+
+# Make sure it works
+random_image_tensor = torch.randn(1, 3, 224, 224)
+vit = ViT(num_classes=len(class_names))
+vit(random_image_tensor)
+
+
+##### TRAIN MODEL!
+
+from going_modular.going_modular import engine
+
+# Adam optimizer
+optimizer = torch.optim.Adam(
+    params=vit.parameters(),
+    lr=3e-3,
+    betas=(0.9, 0.999),
+    weight_decay=0.3
+)
+
+# Loss function
+loss_fn = torch.nn.CrossEntropyLoss()
+
+# Train
+results = engine.train(
+    model=vit,
+    train_dataloader=train_dataloader,
+    test_dataloader=test_dataloader,
+    optimizer=optimizer,
+    loss_fn=loss_fn,
+    epochs=10,
+    device=device
+)
+
+from helper_functions import plot_loss_curves
+
+# Plot our ViT model's loss curves
+plot_loss_curves(results)
+
+plt.show()
