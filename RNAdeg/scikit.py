d2a5a0018555237333cdf71dd2291f4ed8c6fa04
isaacvock <isaac.vock@gmail.com>
2025-03-18 21:22:19 -0400

    Learned how to grid search with scikit learn

--- RNAdeg/scikit.py
+++ RNAdeg/scikit.py
@@ -17,6 +17,8 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 RNAdeg_df = pd.read_csv('C:/Users/isaac/Documents/Simon_Lab/EZbakR_Isoforms_and_NMD_Data/Figure3/Filtered_kdeg_feature_table.csv')
 
@@ -85,3 +87,53 @@ plt.ylabel('Predicted Values')
 plt.title('Actual vs. Predicted Values (AdaBoost)')
 plt.show()
 
+
+### Optimize hyperparameters for Adaboost
+
+ada2 = AdaBoostRegressor(
+    DecisionTreeRegressor(max_depth=4)
+)
+
+nests = list(range(50, 600, 50))
+
+parameters = {
+    'n_estimators' : nests,
+    'learning_rate': list(10.0 ** np.linspace(start = -3,
+                                         stop = 1,
+                                         num = len(nests)))
+}
+
+# adacv = GridSearchCV(ada2, parameters)
+
+# adacv.fit(X_train, y_train)
+
+# adacv.best_params_
+
+ada = AdaBoostRegressor(
+    DecisionTreeRegressor(max_depth=4),
+    learning_rate = 0.25,
+    n_estimators= 200
+)
+
+
+ada.fit(X_train, y_train)
+
+# Make predictions
+y_pred_ada = ada.predict(X_test)
+
+# Evalulate
+mse_ada = mean_squared_error(y_test, y_pred_ada)
+
+print("AdaBoost Optimized Performance:")
+print("MSE =", mse_ada)
+
+# Plot
+plt.figure()
+plt.scatter(y_test, y_pred_ada)
+min_val = min(min(y_test), min(y_pred_ada))
+max_val = max(max(y_test), max(y_pred_ada))
+plt.plot([min_val, max_val], [min_val, max_val])
+plt.xlabel('Actual Values')
+plt.ylabel('Predicted Values')
+plt.title('Actual vs. Predicted Values (AdaBoost)')
+plt.show()
