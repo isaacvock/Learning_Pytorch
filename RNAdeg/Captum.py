04a9ca20147a5d3130f4b2bf602740ccd474d739
isaacvock <isaac.vock@gmail.com>
2025-04-10 22:01:08 -0400

    Started studying Captum and model interpretation

---
+++ RNAdeg/Captum.py
@@ -0,0 +1,50 @@
+### PURPOSE OF THIS SCRIPT
+## Practice using Captum to interpret ML models
+
+
+##### CAPTUM BASIC TEST #####
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from captum.attr import IntegratedGradients
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lin1 = nn.Linear(3, 3)
+        self.relu = nn.ReLU()
+        self.lin2 = nn.Linear(3, 2)
+
+        # Initialize weights and biases
+        self.lin1.weight = nn.Parameter(torch.arange(-4.0, 5.0).view(3, 3))
+        self.lin1.bias = nn.Parameter(torch.zeros(1, 3))
+        self.lin2.weight = nn.Parameter(torch.arange(-3.0, 3.0).view(2, 3))
+        self.lin2.bias =  nn.Parameter(torch.ones(1, 2))
+
+    def forward(self, input):
+        return self.lin2(self.relu(self.lin1(input)))
+    
+model = ToyModel()
+model.eval()
+
+testlin = nn.Linear(3, 2)
+testlin.weight.dim
+
+torch.manual_seed(123)
+np.random.seed(123)
+
+input = torch.rand(2, 3)
+baseline = torch.zeros(2, 3)
+
+ig = IntegratedGradients(model)
+attributions, delta = ig.attribute(
+    input,
+    baseline,
+    target = 0,
+    return_convergence_delta=True
+)
+
+print('IG Attributions', attributions)
+print('Convergence Delta:', delta)
